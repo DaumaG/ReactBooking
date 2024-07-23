27ee427471dcadbd8f392c72a4b310609801255f
Daumantas <milda.tubyte@gmail.com>
2024-07-23 19:48:50 +0300

    fixed logout bug

--- frontend/src/components/bookedServices/BookedList.tsx
+++ frontend/src/components/bookedServices/BookedList.tsx
@@ -5,37 +5,39 @@ import BookedCard from "./BookedCard";
 import { useBookingsGet } from "../booking/hooks";
 import { UserContext } from "@/context/UserContext";
 import { useContext } from "react";
-
-
+ 
+ 
 const GetBookings = () => {
   const { user } = useContext(UserContext);
   if (!user?.email){
     return [];
   }
-
-  console.log("user meail tyea");
+ 
   const { data } = useBookingsGet(user?.email ?? "");
-  console.log(data);
-  
+ 
   return data;
 }
 const BookedList = () => {
+  const { user } = useContext(UserContext);
+  if (!user){
+    return <div>Unauthorized</div>
+  }
+  
   const myBookings = GetBookings() ?? [];
   const { data } = useBusinesses();
   const businesses = data ?? [];
-
-  return (  
+ 
+  return (
     <div className={classNames(styles.container)}>  
-      {myBookings.map((booking) => {  
+      {myBookings.map((booking, i) => {  
         const businessDetails = businesses.find(  
           (business) => business._id === booking.businessId  
         );  
-  
-        // Only render a BookedCard if business details are found  
+ 
         return (  
           businessDetails && (  
             <BookedCard  
-              key={booking.businessId} // Use booking ID or a combination of booking ID and business ID  
+              key={i}
               business={businessDetails}
               booking={booking}
             />  
@@ -45,5 +47,5 @@ const BookedList = () => {
     </div>  
   );  
 };  
-
+ 
 export default BookedList;
